443ba01eaa514c48faeb4b8146beda10baed48b3
Javier Arce <javierarce@gmail.com>
2013-08-08 08:50:44 +0200

    Adds fonts

--- config.rb
+++ config.rb
@@ -5,6 +5,7 @@ http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "img"
+fonts_dir = "fonts"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
