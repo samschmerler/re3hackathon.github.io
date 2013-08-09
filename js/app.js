d5669d7f0587e8a73cd1acbda67cf93dad956d61
Javier Arce <javierarce@gmail.com>
2013-08-09 13:46:31 +0200

    Hover

--- js/app.js
+++ js/app.js
@@ -13,6 +13,8 @@ $(function() {
     e.preventDefault();
     e.stopPropagation();
 
+    $("nav").removeClass("fixed");
+
     var t = $(this).attr("href"),
     n = t.substring(t.indexOf("#") + 1);
 
